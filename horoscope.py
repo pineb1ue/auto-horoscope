d2c1537887723ded8819b0125156d1a90e44aa8f
Mamoru Matsunaga <pineb1ue10ngtree@gmail.com>
2023-12-03 16:52:22 +0900

    feat: add sign

--- horoscope.py
+++ horoscope.py
@@ -2,76 +2,99 @@ import datetime
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import swisseph as swe
 from pytz import timezone
 
 
-def plot_polar_coordinates(planet_names, planet_longitudes):
-    # 描画の準備
-    fig, ax = plt.subplots(subplot_kw={'projection': 'polar'}, figsize=(8, 8))
+def plot_polar_coordinates(planet_names: list[str], planet_longitudes: list[float]):
+    fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=(8, 8))
 
-    # 各惑星の座標を極座標系でプロット
     for name, longitude in zip(planet_names, planet_longitudes):
         radian_angle = np.radians(longitude)
-        ax.plot(radian_angle, 1, 'o', label=name)
+        ax.plot(radian_angle, 1, "o", label=name)
 
-    # グラフを整える
-    ax.set_rmax(1.2)  # 半径の最大値
+    ax.set_rmax(1.2)
     ax.set_rgrids([])
     ax.set_thetagrids(range(0, 360, 30))
     ax.set_title("Horoscope")
 
-    # 凡例を表示
     ax.legend()
-
-    # 描画を表示
     plt.show()
 
 
-def calculate_planet_position(date_time, latitude, longitude, planet_id):
-    jd_utc = swe.julday(date_time.year, date_time.month, date_time.day,
-                        date_time.hour + date_time.minute / 60.0 + date_time.second / 3600.0)
-
+def calculate_planet_position(dt: datetime.datetime, lat: float, lon: float, planet_id: int) -> list[float]:
+    jd_utc = swe.julday(dt.year, dt.month, dt.day, dt.hour + dt.minute / 60.0 + dt.second / 3600.0)
     flag = swe.FLG_SWIEPH | swe.FLG_SPEED
-
-    swe.set_topo(latitude, longitude, 0.0)
-
-    planet_position, _ = swe.calc_ut(jd_utc, planet_id, flag)
+    swe.set_topo(lat, lon, 0.0)
     topocentric_position, _ = swe.calc_ut(jd_utc, planet_id, flag | swe.FLG_TOPOCTR)
+    return topocentric_position
 
-    return planet_position, topocentric_position
+
+def load_data(path: str) -> pd.DataFrame:
+    return pd.read_csv(path)
+
+
+def input_your_birth_and_location(
+    yyyy: int,
+    mm: int,
+    dd: int,
+    HH: int,
+    MM: int,
+    latitude: float = 36.4000,
+    longitude: float = 139.4600,
+) -> tuple[datetime.datetime, float, float]:
+    input_datetime = datetime.datetime(yyyy, mm, dd, HH, MM, 0)
+    input_datetime_jst = input_datetime.astimezone(timezone("Asia/Tokyo"))
+    input_datetime_utc = input_datetime_jst.astimezone(timezone("UTC"))
+    return input_datetime_utc, latitude, longitude
 
 
 def main():
-    input_date_time = datetime.datetime(1997, 12, 30, 23, 20, 0)
-    input_date_time_jst = input_date_time.astimezone(timezone('Asia/Tokyo'))
-    input_date_time_utc = input_date_time_jst.astimezone(timezone('UTC'))
-
-    input_latitude = 36.4000  # Example latitude (Tokyo)
-    input_longitude = 139.4600  # Example longitude (Tokyo)
+    df = load_data("data/desc_sign.csv")
+    your_birth, input_latitude, input_longitude = input_your_birth_and_location(1997, 12, 30, 23, 20)
 
     planet_dict = {
-        "Sun": swe.SUN,
-        "Moon": swe.MOON,
-        "Mercury": swe.MERCURY,
-        "Venus": swe.VENUS,
-        "Mars": swe.MARS,
-        "Jupiter": swe.JUPITER,
-        "Saturn": swe.SATURN,
-        "Uranus": swe.URANUS,
-        "Neptune": swe.NEPTUNE,
-        "Pluto": swe.PLUTO,
+        "太陽": swe.SUN,
+        "月": swe.MOON,
+        "水星": swe.MERCURY,
+        "金星": swe.VENUS,
+        "火星": swe.MARS,
+        "木星": swe.JUPITER,
+        "土星": swe.SATURN,
+    }
+    sign_dict = {
+        i: sign
+        for i, sign in enumerate(
+            [
+                "牡羊",
+                "牡牛",
+                "双子",
+                "蟹",
+                "獅子",
+                "乙女",
+                "天秤",
+                "蠍",
+                "射手",
+                "山羊",
+                "水瓶",
+                "魚",
+            ]
+        )
     }
 
+    your_signs = []
     planet_longitudes = []
 
-    for planet_id in planet_dict.values():
-        _, topocentric_position = calculate_planet_position(
-            input_date_time_utc, input_latitude, input_longitude, planet_id)
+    for planet_name, planet_id in planet_dict.items():
+        topocentric_position = calculate_planet_position(your_birth, input_latitude, input_longitude, planet_id)
+        sign_id = int(topocentric_position[0] // 30)
+        your_signs.append(sign_dict[sign_id])
 
-        # Print results
-        print(f"黄経（Ecliptic Longitude）: {topocentric_position[0]}")
         planet_longitudes.append(topocentric_position[0])
+
+    for planet_name, your_sign in zip(planet_dict.keys(), your_signs):
+        print(df[(df["惑星"] == planet_name) & (df["星座"] == your_sign)].values)
 
     plot_polar_coordinates(list(planet_dict.keys()), planet_longitudes)
 
